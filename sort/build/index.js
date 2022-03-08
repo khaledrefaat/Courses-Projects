b5ab6c31cb64c9f3863b0af01d29c175303f8902
khaledrefaat <khaledrefaat08@gmail.com>
2022-03-08 06:11:46 +0200

    initial setup for sort

---
+++ sort/build/index.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log('Hey there');
+console.log('Hey there');
+console.log('Hey there');
---
+++ sort/src/index.ts
@@ -0,0 +1,3 @@
+console.log('Hey there');
+console.log('Hey there');
+console.log('Hey there');
