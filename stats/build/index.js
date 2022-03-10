7584ec3ad729f79bed4c67bff8776dc82f01fb0a
khaledrefaat <khaledrefaat08@gmail.com>
2022-03-10 09:45:17 +0200

    this is the bad code to analyze code

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,5 +1,23 @@
 "use strict";
-function greet(name) {
-    console.log(`Hello ${name}`);
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+const matches = fs_1.default
+    .readFileSync('football.csv', {
+    encoding: 'utf-8',
+})
+    .split('\n')
+    .map((row) => {
+    return row.split(',');
+});
+let manUnitWins = 0;
+for (let match of matches) {
+    if ((match[1] === 'Man United' && match[5] === 'H') ||
+        (match[2] === 'Man United' && match[5] === 'A')) {
+        manUnitWins++;
+    }
 }
-greet('Khaled');
+console.log('-----------------------------------------------------------------------------------');
+console.log(`Man United won: ${manUnitWins}`);
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,5 +1,25 @@
-function greet(name: string) {
-  console.log(`Hello ${name}`);
-}
+import fs from 'fs';
 
-greet('Khaled');
+const matches = fs
+  .readFileSync('football.csv', {
+    encoding: 'utf-8',
+  })
+  .split('\n')
+  .map((row: string): string[] => {
+    return row.split(',');
+  });
+
+let manUnitWins = 0;
+
+for (let match of matches) {
+  if (
+    (match[1] === 'Man United' && match[5] === 'H') ||
+    (match[2] === 'Man United' && match[5] === 'A')
+  ) {
+    manUnitWins++;
+  }
+}
+console.log(
+  '-----------------------------------------------------------------------------------'
+);
+console.log(`Man United won: ${manUnitWins}`);
